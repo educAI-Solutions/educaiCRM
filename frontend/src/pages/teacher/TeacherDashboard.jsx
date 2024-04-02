706e6d3515dd3e2256a2f86117e40eb013902541
Pablo <89890109+PabloGomez-97@users.noreply.github.com>
2024-04-01 22:05:47 -0300

    update vistas banner

--- frontend/src/pages/teacher/TeacherDashboard.jsx
+++ frontend/src/pages/teacher/TeacherDashboard.jsx
@@ -17,9 +17,48 @@ function TeacherDashboard() {
         height: "100vh",
       }}
     >
-      <Container className="mt-5">
-        <h2 className="text-center mb-4">Teacher Dashboard</h2>
-        <br />
+      <Container className="mt-3">
+        <Row className="mb-3">
+          <Col>
+            <Card className="position-relative">
+              <Card.Img
+                variant="top"
+                src="https://bibliotecas.uai.cl/wp-content/uploads/2021/03/fondo-biblioteca-pregrado-santiago.jpg"
+                alt="Banner"
+                className="img-fluid"
+                style={{ maxHeight: "170px", objectFit: "cover" }}
+              />
+              <div
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  backgroundColor: "rgba(0, 0, 0, 0.5)",
+                }}
+              />
+              <Card.ImgOverlay
+                style={{
+                  top: "50%",
+                  transform: "translateY(-50%)",
+                  color: "white",
+                  textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+                }}
+              >
+                <Card.Title
+                  style={{
+                    textAlign: "center",
+                    fontSize: "2rem",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Teacher Dashboard
+                </Card.Title>
+              </Card.ImgOverlay>
+            </Card>
+          </Col>
+        </Row>
         <Row className="justify-content-center">
           <Col xs={12} sm={6} md={4} lg={3}>
             <Card className="text-center shadow mb-3 h-100">
