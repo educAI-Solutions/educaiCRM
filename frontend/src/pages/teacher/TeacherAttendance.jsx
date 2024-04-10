c91b7c178bcd5d141ef8d27ebd98b766f0b8b61b
foncheto <alfopinto@alumnos.uai.cl>
2024-04-09 20:14:59 -0400

    Enabled upload attendance to azure blob storage

--- frontend/src/pages/teacher/TeacherAttendance.jsx
+++ frontend/src/pages/teacher/TeacherAttendance.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function TeacherAttendance() {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -7,14 +8,30 @@ function TeacherAttendance() {
     setSelectedFile(event.target.files[0]);
   };
 
-  const handleUpload = () => {
-    // Aquí puedes manejar la subida del archivo
-    console.log(selectedFile);
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:7070/storage/upload/attendance",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log(response.data);
+    } catch (error) {
+      console.error("Upload error:", error);
+    }
   };
 
   return (
     <div className="d-flex flex-column align-items-center mt-5 justify-content-center">
-      <h2 className="mb-4">Teacher Upload Page</h2>
+      <h2 className="mb-4">Teacher Attendance Page</h2>
       <div className="container">
         <div className="input-group mb-3">
           <input
