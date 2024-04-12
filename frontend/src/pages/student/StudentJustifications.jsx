541be134a4c9e01ac9a255f6541effce55840ca7
foncheto <alfopinto@alumnos.uai.cl>
2024-04-12 02:35:22 -0400

    Added upload form

--- frontend/src/pages/student/StudentJustifications.jsx
+++ frontend/src/pages/student/StudentJustifications.jsx
@@ -1,18 +1,44 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import Select from "react-select";
 import axios from "axios";
 import { UserContext } from "../../App";
 
 function StudentJustifications() {
   const { id } = useContext(UserContext);
+  const [classes, setClasses] = useState([]);
+  const [selectedClasses, setSelectedClasses] = useState([]);
   const [formData, setFormData] = useState({
-    id: id,
-    fullName: "",
+    student: id,
+    classes: [],
+    fullname: "",
     rut: "",
-    absenceDate: "",
-    justification: "",
+    reason: "",
+    startDate: "",
+    endDate: "",
     file: null,
   });
+
+  useEffect(() => {
+    fetchClasses();
+  }, []);
+
+  const fetchClasses = async () => {
+    try {
+      // Send a GET request to the API
+      const response = await axios.get(
+        `http://localhost:5050/api/classes/get/student/${id}`
+      );
+
+      if (response.status === 200) {
+        setClasses(response.data.data);
+      } else {
+        console.error("Error fetching classes:", response);
+      }
+    } catch (error) {
+      console.error("Error sending request to the API:", error);
+    }
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -23,12 +49,24 @@ function StudentJustifications() {
     setFormData({ ...formData, file: e.target.files[0] });
   };
 
+  const handleClassesChange = (selectedOptions) => {
+    // save only an array of the selected classes' IDs
+    setSelectedClasses(selectedOptions);
+    setFormData({
+      ...formData,
+      classes: selectedOptions.map((cls) => cls.value),
+    });
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
       // Send a POST request to create a new justification
-      const response = await axios.post("/api/justifications", formData);
+      const response = await axios.post(
+        "http://localhost:5050/api/justifications/create",
+        formData
+      );
 
       if (response.status === 200) {
         console.log("Justification created:", response.data);
@@ -68,37 +106,60 @@ function StudentJustifications() {
                 <Form.Control
                   type="text"
                   placeholder="Enter your full name"
-                  name="fullName"
-                  value={formData.fullName}
+                  name="fullname"
+                  value={formData.fullname}
                   onChange={handleChange}
                 />
               </Form.Group>
               <Form.Group controlId="formRut">
-                <Form.Label>DNI</Form.Label>
+                <Form.Label>RUT</Form.Label>
                 <Form.Control
                   type="text"
-                  placeholder="Enter your DNI"
+                  placeholder="Enter your RUT"
                   name="rut"
                   value={formData.rut}
                   onChange={handleChange}
                 />
               </Form.Group>
-              <Form.Group controlId="formEmail">
-                <Form.Label>Email Address</Form.Label>
+              <Form.Group controlId="formReason">
+                <Form.Label>Reason</Form.Label>
                 <Form.Control
-                  type="email"
-                  placeholder="Enter your email address"
-                  name="email"
-                  value={formData.email}
+                  as="textarea"
+                  rows={3}
+                  placeholder="Enter your reason"
+                  name="reason"
+                  value={formData.reason}
                   onChange={handleChange}
                 />
               </Form.Group>
-              <Form.Group controlId="formAbsenceDate">
-                <Form.Label>Absence Date</Form.Label>
+              <Form.Group controlId="formClasses">
+                <Form.Label>Select Classes</Form.Label>
+                <Select
+                  isMulti
+                  value={selectedClasses}
+                  onChange={handleClassesChange}
+                  options={classes.map((cls) => ({
+                    value: cls._id,
+                    label: cls.name,
+                  }))}
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId="formStartDate">
+                <Form.Label>Start Date</Form.Label>
                 <Form.Control
                   type="date"
-                  name="absenceDate"
-                  value={formData.absenceDate}
+                  name="startDate"
+                  value={formData.startDate}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="formEndDate">
+                <Form.Label>End Date</Form.Label>
+                <Form.Control
+                  type="date"
+                  name="endDate"
+                  value={formData.endDate}
                   onChange={handleChange}
                 />
               </Form.Group>
@@ -108,16 +169,6 @@ function StudentJustifications() {
                   type="file"
                   name="justificationFile"
                   onChange={handleFileChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="formJustificationText">
-                <Form.Label>Write Justification</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  name="justification"
-                  value={formData.justification}
-                  onChange={handleChange}
                 />
               </Form.Group>
               <div className="text-center">
