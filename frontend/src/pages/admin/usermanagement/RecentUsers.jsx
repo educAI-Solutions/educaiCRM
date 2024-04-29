6921da0cafc150697155e64fd2e7701bb8bc11f9
foncheto <alfopinto@alumnos.uai.cl>
2024-04-29 01:10:45 -0400

    Added edit role

--- frontend/src/pages/admin/usermanagement/RecentUsers.jsx
+++ frontend/src/pages/admin/usermanagement/RecentUsers.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Table, Button } from "react-bootstrap";
+import { Table, Button, Modal, Form } from "react-bootstrap";
 import { useTranslation } from "react-i18next"; // Import useTranslation hook from react-i18next
 import axios from "axios"; // Import Axios for making HTTP requests
 
 function RecentUsers() {
   const [users, setUsers] = useState([]);
   const { t } = useTranslation(); // Initialize the useTranslation hook
+  const [showModal, setShowModal] = useState(false);
+  const [editingUser, setEditingUser] = useState(null);
 
   useEffect(() => {
     // Fetch the users from your API here and set the users state
@@ -36,52 +38,134 @@ function RecentUsers() {
     }
   };
 
-  const editRole = (userId, newRole) => {
-    // Edit the role of the user with the given userId here and update the users state
+  const editRole = async () => {
+    handleCloseModal();
+    const updatedRole = document.getElementById("formRole").value;
+    const updatedUser = { ...editingUser, role: updatedRole };
+    try {
+      // Make an API call to update the user role
+      await axios.put(
+        `http://localhost:5050/api/auth/updateRole/${editingUser._id}`,
+        updatedUser
+      );
+
+      // Update the users state by mapping over the users and updating the user with the new role
+      setUsers(
+        users.map((user) => (user._id === updatedUser._id ? updatedUser : user))
+      );
+    } catch (error) {
+      console.error("Edit user role error:", error);
+    }
+  };
+  const handleOpenModal = (user) => {
+    setEditingUser(user);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setEditingUser(null);
+    setShowModal(false);
   };
 
   return (
-    <Table responsive>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>{t("adminDashboard.usersManagement.recentTable.username")}</th>
-          <th>{t("adminDashboard.usersManagement.recentTable.email")}</th>
-          <th>{t("adminDashboard.usersManagement.recentTable.role")}</th>
-          <th>{t("adminDashboard.usersManagement.recentTable.actions")}</th>
-        </tr>
-      </thead>
-      {users.length === 0 && (
-        <tbody>
+    <div>
+      <Table responsive>
+        <thead>
           <tr>
-            <td colSpan="5" className="text-center">
-              {t("adminDashboard.usersManagement.recentTable.notfound")}
-            </td>
+            <th>#</th>
+            <th>{t("adminDashboard.usersManagement.recentTable.username")}</th>
+            <th>{t("adminDashboard.usersManagement.recentTable.email")}</th>
+            <th>{t("adminDashboard.usersManagement.recentTable.role")}</th>
+            <th>{t("adminDashboard.usersManagement.recentTable.actions")}</th>
           </tr>
+        </thead>
+        {users.length === 0 && (
+          <tbody>
+            <tr>
+              <td colSpan="5" className="text-center">
+                {t("adminDashboard.usersManagement.recentTable.notfound")}
+              </td>
+            </tr>
+          </tbody>
+        )}
+        <tbody>
+          {users.slice(0, 5).map((user, index) => (
+            <tr key={user._id}>
+              <td>{index + 1}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.role}</td>
+              <td>
+                <Button variant="danger" onClick={() => deleteUser(user._id)}>
+                  {t("adminDashboard.usersManagement.recentTable.delete")}
+                </Button>
+                <Button variant="primary" onClick={() => handleOpenModal(user)}>
+                  {t("adminDashboard.usersManagement.recentTable.edit")}
+                </Button>
+              </td>
+            </tr>
+          ))}
         </tbody>
-      )}
-      <tbody>
-        {users.slice(0, 5).map((user, index) => (
-          <tr key={user._id}>
-            <td>{index + 1}</td>
-            <td>{user.username}</td>
-            <td>{user.email}</td>
-            <td>{user.role}</td>
-            <td>
-              <Button variant="danger" onClick={() => deleteUser(user._id)}>
-                {t("adminDashboard.usersManagement.recentTable.delete")}
-              </Button>
-              <Button
-                variant="primary"
-                onClick={() => editRole(user._id, "newRole")}
-              >
-                {t("adminDashboard.usersManagement.recentTable.edit")}
-              </Button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+      </Table>
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {t("adminDashboard.usersManagement.modalEditUser.title")}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formUsername">
+              <Form.Label>
+                {" "}
+                {t("adminDashboard.usersManagement.modalEditUser.username")}
+              </Form.Label>
+              <Form.Control
+                type="text"
+                defaultValue={editingUser?.username}
+                readOnly
+              />
+            </Form.Group>
+            <Form.Group controlId="formEmail">
+              <Form.Label>
+                {" "}
+                {t("adminDashboard.usersManagement.modalEditUser.email")}
+              </Form.Label>
+              <Form.Control
+                type="email"
+                defaultValue={editingUser?.email}
+                readOnly
+              />
+            </Form.Group>
+            <Form.Group controlId="formRole">
+              <Form.Label>
+                {" "}
+                {t("adminDashboard.usersManagement.modalEditUser.role")}
+              </Form.Label>
+              <Form.Control as="select" defaultValue={editingUser?.role}>
+                <option value="admin">
+                  {t("adminDashboard.usersManagement.modalEditUser.admin")}
+                </option>
+                <option value="teacher">
+                  {t("adminDashboard.usersManagement.modalEditUser.teacher")}
+                </option>
+                <option value="student">
+                  {t("adminDashboard.usersManagement.modalEditUser.student")}
+                </option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            {t("adminDashboard.usersManagement.modalEditUser.close")}
+          </Button>
+          <Button variant="primary" onClick={editRole}>
+            {t("adminDashboard.usersManagement.modalEditUser.submit")}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   );
 }
 
--- frontend/src/pages/admin/usermanagement/SearchUser.jsx
+++ frontend/src/pages/admin/usermanagement/SearchUser.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { Table, Form, Button, InputGroup, FormControl } from "react-bootstrap";
+import {
+  Table,
+  Form,
+  Button,
+  InputGroup,
+  FormControl,
+  Modal,
+} from "react-bootstrap";
 import axios from "axios";
 import { useTranslation } from "react-i18next";
 
@@ -8,6 +15,8 @@ function SearchUser() {
   const [user, setUser] = useState(null);
   const [users, setUsers] = useState([]);
   const { t } = useTranslation();
+  const [showModal, setShowModal] = useState(false);
+  const [editingUser, setEditingUser] = useState(null);
 
   const handleSearch = async (event) => {
     event.preventDefault();
@@ -36,8 +45,33 @@ function SearchUser() {
     }
   };
 
-  const editUser = (userId) => {
-    // Edit the user with the given userId here and update the users state
+  const editRole = async () => {
+    handleCloseModal();
+    const updatedRole = document.getElementById("formRole").value;
+    const updatedUser = { ...editingUser, role: updatedRole };
+    try {
+      // Make an API call to update the user role
+      await axios.put(
+        `http://localhost:5050/api/auth/updateRole/${editingUser._id}`,
+        updatedUser
+      );
+
+      // Update the users state by mapping over the users and updating the user with the new role
+      setUsers(
+        users.map((user) => (user._id === updatedUser._id ? updatedUser : user))
+      );
+    } catch (error) {
+      console.error("Edit user role error:", error);
+    }
+  };
+  const handleOpenModal = (user) => {
+    setEditingUser(user);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setEditingUser(null);
+    setShowModal(false);
   };
 
   return (
@@ -80,7 +114,7 @@ function SearchUser() {
                 <Button variant="danger" onClick={() => deleteUser(user._id)}>
                   {t("adminDashboard.usersManagement.searchTable.delete")}
                 </Button>
-                <Button variant="primary" onClick={() => editUser(user._id)}>
+                <Button variant="primary" onClick={() => handleOpenModal(user)}>
                   {t("adminDashboard.usersManagement.searchTable.edit")}
                 </Button>
               </td>
@@ -88,6 +122,64 @@ function SearchUser() {
           </tbody>
         </Table>
       )}
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {t("adminDashboard.usersManagement.modalEditUser.title")}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formUsername">
+              <Form.Label>
+                {" "}
+                {t("adminDashboard.usersManagement.modalEditUser.username")}
+              </Form.Label>
+              <Form.Control
+                type="text"
+                defaultValue={editingUser?.username}
+                readOnly
+              />
+            </Form.Group>
+            <Form.Group controlId="formEmail">
+              <Form.Label>
+                {" "}
+                {t("adminDashboard.usersManagement.modalEditUser.email")}
+              </Form.Label>
+              <Form.Control
+                type="email"
+                defaultValue={editingUser?.email}
+                readOnly
+              />
+            </Form.Group>
+            <Form.Group controlId="formRole">
+              <Form.Label>
+                {" "}
+                {t("adminDashboard.usersManagement.modalEditUser.role")}
+              </Form.Label>
+              <Form.Control as="select" defaultValue={editingUser?.role}>
+                <option value="admin">
+                  {t("adminDashboard.usersManagement.modalEditUser.admin")}
+                </option>
+                <option value="teacher">
+                  {t("adminDashboard.usersManagement.modalEditUser.teacher")}
+                </option>
+                <option value="student">
+                  {t("adminDashboard.usersManagement.modalEditUser.student")}
+                </option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            {t("adminDashboard.usersManagement.modalEditUser.close")}
+          </Button>
+          <Button variant="primary" onClick={editRole}>
+            {t("adminDashboard.usersManagement.modalEditUser.submit")}
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
